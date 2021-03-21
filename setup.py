a35361995950f8ee32b36432d6377c0249dd5051
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-21 14:53:52 +0300

    add setup.py and requrements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import setuptools
+
+requirements_file = Path(__file__).parent / "requirements.txt"
+with open(requirements_file) as f:
+    install_requires = f.read().strip().split()
+
+setuptools.setup(
+    name="src",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+)
