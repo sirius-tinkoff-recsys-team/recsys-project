e702e5c697554e3dde2093c62abfd2bd9fba7ec1
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-21 15:52:06 +0300

    move directory finder to utils

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ from box import Box
 from box.converters import yaml
 
 from src.monitoring import report, shorten_path
+from src.utils.fs import find_parent_containing
 
 def _join_path(loader, node):
     seq = loader.construct_sequence(node)
@@ -16,21 +17,12 @@ def _get_from_env(loader, node):
 yaml.SafeLoader.add_constructor('!join_path', _join_path)
 yaml.SafeLoader.add_constructor('!env', _get_from_env)
 
-def _get_file_path(filename):
-    path = Path(".").resolve()
-    while not path == path.root:
-        file_path = path / filename
-        if file_path.exists():
-            return file_path
-        path = path.parent
-    return None
-
 params = Box(box_dots=True)
 config = Box(box_dots=True)
 
 def load_configs():
     for name in ["params", "config"]:
-        filename = _get_file_path(f"{name}.yaml")
+        filename = find_parent_containing(f"{name}.yaml", return_parent=False)
         try:
             loaded = Box.from_yaml(
                 filename=filename,
