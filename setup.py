94af5d956f10cb1f9d2e3e5e8b0d7b867d6159d8
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-23 10:28:51 +0300

    create a separate requirements list in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from pathlib import Path
 import setuptools
-
-requirements_file = Path(__file__).parent / "requirements.txt"
-with open(requirements_file) as f:
-    install_requires = f.read().strip().split("\n")
 
 setuptools.setup(
     name="src",
     packages=setuptools.find_packages(),
-    # install_requires=install_requires,
+    install_requires=[
+        "attrs",
+        "python-box",
+        "rich",
+        "pandarallel",
+        "haversine",
+        "torch",
+        "pytorch_lightning",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "dvclive",
+        "neptune-client",
+    ],
 )
